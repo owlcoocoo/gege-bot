b1569494bbcc2f4122ee803e09d437ec4b8b2637
owlcoocoo <959660273@qq.com>
2023-09-17 14:53:23 +0800

    add sqlite

---
+++ src/GegeBot/Db/IDb.cs
@@ -0,0 +1,9 @@
+﻿namespace GegeBot.Db
+{
+    public interface IDb
+    {
+        string GetValue(string key);
+        bool SetValue(string key, string value);
+        bool DeleteKey(string key);
+    }
+}
