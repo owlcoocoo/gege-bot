45e20c09b83370ea58401bd76583be47deca3497
owlcoocoo <959660273@qq.com>
2024-01-12 21:24:40 +0800

    戳一戳随机图片

---
+++ src/GegeBot/Plugins/Poke/PokeConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace GegeBot.Plugins.Poke
+{
+    [Config("Poke")]
+    internal class PokeConfig
+    {
+        /// <summary>
+        /// 存放文件的路径
+        /// </summary>
+        public static string FilePath { get; set; }
+    }
+}
---
+++ src/GegeBot/Plugins/Poke/PokeHandler.cs
@@ -0,0 +1,41 @@
+﻿using CQHttp;
+using CQHttp.DTOs;
+
+namespace GegeBot.Plugins.Poke
+{
+    [Plugin]
+    internal class PokeHandler
+    {
+        readonly CQBot cqBot;
+
+        public PokeHandler(CQBot bot)
+        {
+            cqBot = bot;
+            cqBot.ReceivedPoke += CqBot_ReceivedPoke; ;
+        }
+
+        List<string> GetFiles()
+        {
+            string[] extensions = [".png", ".jpg", ".jpeg", ".gif"];
+            return Directory.EnumerateFiles(PokeConfig.FilePath, "*.*").Where(x => extensions.Any(a => x.ToLower().EndsWith(a))).ToList();
+        }
+
+        private void CqBot_ReceivedPoke(CQEventPoke obj)
+        {
+            if (obj.target_id.ToString() != cqBot.BotID) return;
+            var files = GetFiles();
+            if (files.Count < 1) return;
+            int n = new Random().Next(files.Count - 1);
+            byte[] data = File.ReadAllBytes(files[n]);
+            var cqCode = new CQCode().SetImage(data, true);
+            if (obj.group_id != 0)
+            {
+                cqBot.Message_SendGroupMsg(obj.group_id, cqCode);
+            }
+            else
+            {
+                cqBot.Message_SendPrivateMsg(obj.user_id, cqCode);
+            }
+        }
+    }
+}
